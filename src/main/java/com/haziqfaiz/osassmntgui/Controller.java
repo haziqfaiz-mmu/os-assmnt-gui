935fce7d072014bbd611ac7849e051f06b2414fc
Haziq Faiz <haziq.faiz@gmail.com>
2021-10-24 17:49:59 +0800

    "Update combo box"

--- src/main/java/com/haziqfaiz/osassmntgui/Controller.java
+++ src/main/java/com/haziqfaiz/osassmntgui/Controller.java
@@ -1,14 +1,21 @@
 package com.haziqfaiz.osassmntgui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class Controller implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox algorithmBox;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
